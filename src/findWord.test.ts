460a6feed28e34a825820a0c9796042c0c67ccaf
Ian Gilham <igilham@users.noreply.github.com>
2023-10-21 14:31:34 +0100

    refactor: remove errors from string finder utils

--- src/findWord.test.ts
+++ src/findWord.test.ts
@@ -2,13 +2,26 @@ import { findChar, findWord } from "./findWord";
 
 describe("findWord", () => {
   test("can extract chars from a string", () => {
-    expect(findChar("a", "aaaa", 1)).toBe("aaa");
-    expect(findChar("a", "aaaa", 2)).toBe("aa");
-    expect(findChar("a", "aaaa", 3)).toBe("a");
-    expect(findChar("a", "aaaa", 4)).toBe("");
-    expect(() => findChar("a", "aaaa", 5)).toThrow(
-      "failed to find 5th duplicate"
-    );
+    expect(findChar("a", "aaaa", 1)).toEqual({
+      found: true,
+      remainingChars: "aaa",
+    });
+    expect(findChar("a", "aaaa", 2)).toEqual({
+      found: true,
+      remainingChars: "aa",
+    });
+    expect(findChar("a", "aaaa", 3)).toEqual({
+      found: true,
+      remainingChars: "a",
+    });
+    expect(findChar("a", "aaaa", 4)).toEqual({
+      found: true,
+      remainingChars: "",
+    });
+    expect(findChar("a", "aaaa", 5)).toEqual({
+      found: false,
+      remainingChars: "aaaa",
+    });
   });
 
   test("can find a word and slice the remaining chars", () => {
@@ -18,6 +31,20 @@ describe("findWord", () => {
 
   test("can find a word and double slice the remaining chars", () => {
     const result = findWord("rose", "eeeeddoonnnsssv", 2, "r");
+    expect(result).toEqual({ word: "rose", remainingChars: "eeddnnnsv" });
+  });
+
+  // normal end-to-end usage
+  test("can find a word by extracting the first char(s) once then the rest twice", () => {
+    const word = "rose";
+    const input = "eeeeddoonnnsssrv";
+    const remAfterFirst = findChar(word.charAt(0), input, 1);
+    const result = findWord(
+      word,
+      remAfterFirst.remainingChars,
+      2,
+      word.charAt(0)
+    );
     expect(result).toEqual({ word: "rose", remainingChars: "eeddnnnsv" });
   });
 });
--- src/findWord.ts
+++ src/findWord.ts
@@ -1,17 +1,22 @@
 // find a char n times in a string, removing it from the string each time, then return the remaining string.
-export function findChar(char: string, inputChars: string, n: number): string {
+export function findChar(
+  char: string,
+  inputChars: string,
+  n: number
+): { found: boolean; remainingChars: string } {
   let remainingChars = inputChars;
   for (let i = 0; i < n; i++) {
     const pos = remainingChars.indexOf(char);
     if (pos === -1) {
-      throw Error(`failed to find ${i + 1}th duplicate`);
+      // throw Error(`failed to find ${i + 1}th duplicate`);
+      return { found: false, remainingChars: inputChars };
     }
     remainingChars = remainingChars
       .slice(0, pos)
       .concat(remainingChars.slice(pos + 1));
   }
 
-  return remainingChars;
+  return { found: true, remainingChars };
 }
 
 // find a word in a string, removing the chars charCopies times then return the word and the remaining string
@@ -25,16 +30,16 @@ export function findWord(
   remainingChars: string;
 } {
   const nextChar = word.charAt(acc.length);
-  try {
-    const remainingChars = findChar(nextChar, inputChars, charCopies);
-    const found = acc.concat(nextChar);
-    if (found === word) {
-      return { word: found, remainingChars };
-    }
 
-    // warning: node does not implement tail recursion - max depth is around 512 stack entries
-    return findWord(word, remainingChars, charCopies, found);
-  } catch (e) {
+  const foundChar = findChar(nextChar, inputChars, charCopies);
+  if (!foundChar.found) {
     return { word: "", remainingChars: inputChars };
   }
+  const found = acc.concat(nextChar);
+  if (found === word) {
+    return { word: found, remainingChars: foundChar.remainingChars };
+  }
+
+  // warning: node does not implement tail recursion - max depth is around 512 stack entries
+  return findWord(word, foundChar.remainingChars, charCopies, found);
 }
