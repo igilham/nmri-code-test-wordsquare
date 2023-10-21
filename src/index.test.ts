cb86fca18c486f6af691b475f47e496ff76c8809
Ian Gilham <igilham@users.noreply.github.com>
2023-10-21 12:39:33 +0100

    feat: fail parsing if input is incorrect length

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,16 @@
 import { WordSquare } from "./index";
 
 describe("WordSquare", () => {
-  test("parses the input", () => {
+  test("parses 2x2 input", () => {
     const ws = WordSquare.parse(2, "aaaa");
     expect(ws.size).toBe(2);
     expect(ws.input).toBe("aaaa");
   });
+
+  test.each([{ size: 2, input: "aaaaa" }])(
+    "does not parse input if the string is the wrong length",
+    ({ size, input }) => {
+      expect(() => WordSquare.parse(size, input)).toThrow("Invalid input");
+    }
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export class WordSquare {
   static parse(size: number, input: string): WordSquare {
+    if (input.length !== size * size) {
+      throw new Error("Invalid input");
+    }
     return new WordSquare(size, input);
   }
 
