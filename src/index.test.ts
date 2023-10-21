77262ce74e66add4d3d3c03236d58c6135026984
Ian Gilham <igilham@users.noreply.github.com>
2023-10-21 12:36:17 +0100

    test: add simplistic assertions to basic construction test

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,5 +3,7 @@ import { WordSquare } from "./index";
 describe("WordSquare", () => {
   test("parses the input", () => {
     const ws = WordSquare.parse(2, "aaaa");
+    expect(ws.size).toBe(2);
+    expect(ws.input).toBe("aaaa");
   });
 });
