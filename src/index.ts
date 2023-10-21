ba0f2ee4dbc7047081443a8471e30f047fc47f8f
Ian Gilham <igilham@users.noreply.github.com>
2023-10-21 15:16:24 +0100

    feat: add a CLI app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-export { WordSquare } from "./WordSquare";
+import { WordSquare } from "./WordSquare";
+
+import { Command } from "commander";
+
+const program = new Command();
+program
+  .description("Solve a word square puzzle")
+  .argument("<size>", "size of the word square", parseInt)
+  .argument("<input>", "input string")
+  .action((size, input) => {
+    // TODO: implement some sort of dictionary
+    const wordSquare = new WordSquare(size, input, [
+      "ends",
+      "oven",
+      "rose",
+      "send",
+    ]);
+    const solution = wordSquare.solve();
+    for (const word of solution) {
+      console.log(word);
+    }
+  });
+
+program.parse(process.argv);
